20c0e83e0fa52c29ce4344aeb3a92670b501f811
atomatic <mail@martin-lihs.com>
2021-11-10 19:56:32 +0100

    Update

--- src/functionalIcon.js
+++ src/functionalIcon.js
@@ -1,6 +1,12 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
+
+import * as GF from './globalfunctions.js'
+import * as HTML from './htmlframe.js'
+
+
+
 const defaultStyleName = "1. Default"
 const subfolderName = "export"
 
@@ -12,6 +18,14 @@ var fileType = 'csv';
 var document = sketch.getSelectedDocument()
 var layerStyles = document.sharedLayerStyles;
 var UI = require('sketch/ui')
+
+var Shape = require('sketch/dom').Shape
+var Style = require('sketch/dom').Style
+
+
+const errorLayerName = "Point-Error-Marker_Plugin";
+
+
 
 
 
@@ -45,6 +59,12 @@ function saveToFile({ filenamePrefix, content: string }) {
     //alert.runModal();
   }
 }
+
+
+
+
+
+
 
 
 function setSlice(object, exportname){
@@ -83,11 +103,225 @@ function selectPage(theDocument){
       curentpage = value;
     }
   )
-
   return curentpage;
 }
 
 
+function checkShapePaht(object){
+
+  const collisionPrecision = 0.25;
+  var objCollisionPrecision = collisionPrecision;
+  var objFrame = object.frame;
+  var returnError = false;
+
+  if (object.points){
+
+    object.points.forEach(function(obj, i){
+
+
+        if (i > 0) {
+
+          var pointA = object.points[i].point;
+          var pointB = object.points[i-1].point
+          var  objCollusion = GF.checkCollusion(pointA, pointB, objFrame, objCollisionPrecision)
+
+          if (objCollusion){
+
+              GF.drawErrorRegion(pointA, pointB, objFrame, object, errorLayerName);
+
+              returnError = true;
+
+          } else {}
+
+
+        }
+    })
+  }
+
+  return returnError;
+
+}
+
+
+
+
+export var checkHiddenPoints = function(context){
+
+  var selectedPage = selectPage(document)
+
+  
+  var checkOutput = "";
+  var htmlHeader = HTML.header;
+  var htmlFooter = HTML.footer;
+
+  var docName = GF.documentName(context)
+  var docNameArray = docName.split("/");
+  docName = docNameArray[docNameArray.length - 1];
+
+  let htmlDocHeader = "<h1>Icon Design Consistency Check</h1><p class='subline'>"+docName+"</p>"
+  let htmlTableHeader = `<table>
+                        <thead>
+                          <tr>
+                            <th width="200px">Icon</th>
+                            <th>Vector Quality</th>
+                            <th>Vector Closed</th>
+                          </tr>
+                        </thead>`
+
+  function checkSingeleShape (object) {
+    var objName = object.name;
+    var objNameArray = objName.split("/")
+    var objLastName = objNameArray[objNameArray.length -1];
+ 
+    var objArtboard = object.getParentArtboard();
+    var objArtboardName = "No Artboard"
+
+    if (objArtboard){
+      objArtboardName = objArtboard.name;
+    }
+
+    var link = GF.createLink(object.id)
+    objName = "<td><span class='block'>"+GF.createNiceHTMLLink(objLastName, link)+"</span><span class='block smallText'>"+objArtboardName+"</span></td>";
+    
+
+
+    // Check Point Collision
+    var outPutVectorError = "<td>✅</td>"
+    var checkResult = checkShapePaht(object);
+
+    if(checkResult){
+      outPutVectorError = "<td class='error'>Points to Close</td>"
+    }
+
+    var checkCloseResult = object.closed ? 1:0
+
+    var outPutClosed = "<td>✅</td>"
+    if (checkCloseResult == 0){
+      outPutClosed = "<td class='error'>Shape is Open</td>"
+    }
+
+
+    var errorExist = false;
+    if (checkResult || !checkCloseResult) {
+      errorExist = true;
+    }
+
+    return {"error": errorExist, "export": (objName + outPutVectorError + outPutClosed)};
+
+  }
+
+
+
+  
+
+  function deleteAllErrorPoints(object) {
+    
+    if (object.name == "Point-Error-Marker_Plugin"){
+      object.remove();
+    }
+
+    if (object.layers && object.layers.length) {
+      object.layers.forEach(deleteAllErrorPoints);
+    }
+  }
+
+
+
+  function runThruPoints(object) {
+
+    if (object.type == "Shape"){
+
+      var objName = object.name;
+      var objNameArray = objName.split("/")
+      var objLastName = objNameArray[objNameArray.length -1];
+ 
+      var objArtboard = object.getParentArtboard();
+      var objArtboardName = objLastName + "  !No Artboard"
+      var artBoardError = true;
+
+      if (objArtboard){
+        artBoardError = false;
+        var artboardArray = objArtboard.name.split("/");
+        objArtboardName = artboardArray[artboardArray.length-1];
+      }
+      
+      var detailTable = "";
+      var detailError = false;
+
+      detailTable+="<tbody class='table-to-hide'>"
+      object.layers.forEach(shape => {
+        
+        var checkDetailResult = checkSingeleShape(shape)
+
+        if (checkDetailResult.error){
+          detailError = true;
+        }
+
+        detailTable+="<tr>";
+        detailTable+=checkDetailResult.export;
+        detailTable+="</tr>";
+        
+      })
+      detailTable+="</tbody>"
+
+
+      var mainErrorClass = "<tr>"
+      if (detailError){
+        mainErrorClass = "<tr class='error'>"
+      }
+
+      if (artBoardError){
+        mainErrorClass = "<tr class='warning'>"
+      }
+
+      
+
+      checkOutput+="<tbody>"
+      checkOutput+=mainErrorClass;
+      checkOutput+="<td colspan='2'><label class='clicklabel'><input type='radio' name='accountin' class='hideandshow' data-toggle='toggle' onclick='hideAndShowTable(this)'><span>"+objArtboardName +"</span></label><td/>"
+      checkOutput+="</tr>";
+      checkOutput+="</tbody>"
+
+      checkOutput+= detailTable;
+
+
+
+    }  else if (object.type == "ShapePath" && object.parent.type != "Shape"){
+
+      checkOutput+="<tbody>"
+      checkOutput+="<tr>";
+      checkOutput+=checkSingeleShape(object).export;
+      checkOutput+="</tr>";
+      checkOutput+="</tbody>"
+
+    }
+
+
+
+    if (object.layers && object.layers.length) {
+      object.layers.forEach(runThruPoints);
+    }
+
+  }
+
+  //
+
+  document.pages.forEach(deleteAllErrorPoints)
+  document.getLayersNamed(selectedPage).forEach(runThruPoints)
+
+  //document.pages.forEach(runThruPoints)
+
+  var exportToFile = htmlHeader + htmlDocHeader + htmlTableHeader;
+  exportToFile +=checkOutput;
+  exportToFile += htmlFooter;
+
+
+  GF.saveToFile({
+    filenamePrefix: docName+"-Symbol-Structure-Test.html",
+    content: NSString.stringWithString(exportToFile)
+  });
+
+}
 
 
 
--- src/globalfunctions.js
+++ src/globalfunctions.js
@@ -1,4 +1,17 @@
 
+import sketch from 'sketch'
+var document = sketch.getSelectedDocument()
+var layerStyles = document.sharedLayerStyles;
+var UI = require('sketch/ui')
+
+var Shape = require('sketch/dom').Shape
+var Style = require('sketch/dom').Style
+
+
+var FILETYPE = 'html';
+let LINKINPUT = "sketch://plugin/com.atomatic.icon-quality-tools/"
+let LAYERSHOWFUNCTION = "element.show"
+
 
 
 export function saveToFile({ filenamePrefix, content: string, fileType }) {
@@ -30,4 +43,101 @@ export function saveToFile({ filenamePrefix, content: string, fileType }) {
     //alert("Shared Color Palette JSON Exported!", "Styls Not Exprtet");
     //alert.runModal();
   }
+}
+
+
+export function documentName(context){
+
+  if (context.document.fileURL() == null) { 
+    return "unsaved_Doument";
+  } else {
+    return context.document.fileURL().path().replace(/\.sketch$/, '')
+  }
+
+}
+
+
+
+export function createLink(uri){
+  // encodeURIComponent(URI)
+  return LINKINPUT+LAYERSHOWFUNCTION+"?msg="+encodeURIComponent(uri);
+
+}
+
+export function createNiceHTMLLink( niceName,link){
+  return '<a href="'+link+'">'+niceName+'</a>';
+}
+
+
+export function createNiceHTMLName(name){
+  var lastName = name.split("/")
+  lastName = lastName[lastName.length -1];
+  return "<td><span class='block'>"+lastName+"</span><span class='block smallText'>"+name+"</span></td>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function checkCollusion(pointA, pointB, objFrame, objCollisionPrecision) {
+
+  var objX1 = pointA.x * objFrame.width;
+  var objY1 = pointA.y * objFrame.height;
+
+  var objX2 = pointB.x * objFrame.width;
+  var objY2 = pointB.y * objFrame.height; 
+
+  if (objX1 < objX2 + objCollisionPrecision &&
+      objX1 + objCollisionPrecision > objX2 &&
+      objY1 < objY2 + objCollisionPrecision &&
+      objCollisionPrecision + objY1 > objY2) {
+      return true;
+  } else {
+      return false;
+  }
+
+}
+
+
+
+export function drawErrorRegion (pointA, pointB, objFrame, object, name){
+  var fillStyle = {
+                  "borders":
+                    [{
+                      color: '#ec0016',
+                      fillType: Style.FillType.Color,
+                      thickness: 0.5
+                    }]
+                  }
+  var absolutePosX = object.sketchObject.absoluteRect().x() - object.getParentArtboard().sketchObject.absoluteRect().x();
+  var absolutePosY = object.sketchObject.absoluteRect().y() - object.getParentArtboard().sketchObject.absoluteRect().y();
+
+  var newShape = sketch.Shape;
+  var errorPosX = pointA.x*objFrame.width + absolutePosX;
+  var errorPosY = pointA.y*objFrame.height + absolutePosY;
+  var errorDimension = 2;
+
+  
+
+  var mySquare = new newShape({
+    name: name,
+    parent: object.getParentArtboard(), 
+    frame: { x:errorPosX -1, y:errorPosY-1, width: errorDimension, height: errorDimension},
+    style: fillStyle
+  })
+
+
+  mySquare.layers[0].points.forEach(radius => {
+    radius.cornerRadius = errorDimension/2;
+
+  })
 }
